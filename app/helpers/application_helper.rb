f2f1e88c08595a953808a5146015e0b1a38b0f53
jinhster <jinhster@gmail.com>
2014-08-12 23:03:46 -0400

    Updated
    Updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,9 @@ module ApplicationHelper
 			""
 		end
 	end
+
+	def random_background_images
+  		images = ["assets/images/2001.png", "assets/images/1358906983457.jpg", "assets/images/5310355609_c6edb6b888_o.jpg", "assets/images/Animals-Birds.jpg"]
+  		images[rand(images.size)]
+	end
 end
